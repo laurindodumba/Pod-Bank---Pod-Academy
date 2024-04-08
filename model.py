2d83293d187536c3b852d715f8592d14d7da2661
laurindodumba <dumbalvd@gmail.com>
2024-04-07 22:33:40 -0300

    feature: Adicionei a lógica para pedir o crédito

--- model.py
+++ model.py
@@ -5,55 +5,59 @@ import streamlit as st
 # with open('modelo_credito.pkl', 'rb') as file:
 #     model = pkl.load(file)
 
-
-#Criar formulário para os usários
 def formulario():
     genero_list = ('', 'M', 'F')
-
     genero_option = list(range(len(genero_list)))
     genero_escolha = st.selectbox("SEXO", genero_option, format_func=lambda x: genero_list[x])
 
-    # Se tem meios de transporte
     carro_list = ('', 'Y', 'N')
-
     opcoes = list(range(len(carro_list)))
     escolha_carro = st.selectbox("CARRO", opcoes, format_func=lambda x: carro_list[x])
 
-    #nivel  escolar
     nivel_escolar = ('','Secundário', 'Educação Superior')
-
     nivel = list(range(len(nivel_escolar)))
     escolha_nivel = st.selectbox("NÍVEL ESCOLAR", nivel, format_func=(lambda x: nivel_escolar[x]))
 
-    #Estado Civil
     estado_civil = ('','Casado', 'Solteiro', 'Noivo', 'Separado')
     estado = list(range(len(estado_civil)))
     escolha_estado = st.selectbox("ESTADO CIVIL", estado, format_func=(lambda x: estado_civil[x]))
 
-    #Modelo de residência
-    modelo_casa = ('','APARTAMENTO', "CASA", )
+    modelo_casa = ('','APARTAMENTO', 'CASA')
+    casa = list(range(len(modelo_casa)))
+    escolha_modelo = st.selectbox("MODELO DE RESIDÊNCIA", casa, format_func=lambda x: modelo_casa[x])
 
-    # Quantidade do empréstimo
-    duracao = st.number_input("AMT_ANNUITY",value=0)
+    # Renda anual do solicitante
+    renda_anual = st.number_input("Renda Anual", value=0)
 
-    # Tempo de emprestimo
-    tempo =  ['',"3 Meses", "6 Meses", "9 Meses", "12 Meses", "16 Meses", "24 Meses", "48 Meses"]
+    # Quantidade do empréstimo solicitado
+    duracao = st.number_input("Montante do Empréstimo Solicitado", value=0)
+
+    tempo = ['',"3 Meses", "6 Meses", "9 Meses", "12 Meses", "16 Meses", "24 Meses", "48 Meses"]
     tempo_opcoes = range(len(tempo))
     tempo_selecao = st.selectbox("Duração do Empréstimo", tempo_opcoes, format_func=(lambda x: tempo[x]))
 
-
     #Botão para enviar o pedido de crédito
     if st.button("ENVIAR PEDIDO DE CRÉDITO"):
-        # Verifique se todas as seleções foram feitas
-        if not all([genero_escolha, escolha_carro, escolha_nivel, escolha_estado, escolha_modelo, duracao is not None, tempo_selecao]):
-            st.error("Por favor, preencha todos os campos.")
+        # Verifique se todas as seleções foram feitas e se a renda anual é maior que 0
+        if not all([genero_escolha, escolha_carro, escolha_nivel, escolha_estado, escolha_modelo, renda_anual > 0, duracao > 0, tempo_selecao]):
+            st.error("Por favor, preencha todos os campos e assegure-se de que os valores inseridos são válidos.")
         else:
-            # Processo de predição
-            features = [[genero_escolha-1, escolha_carro-1, duracao, escolha_nivel-1, escolha_estado-1, escolha_modelo-1]]
-            prediction = model.predict(features)
+            # Calculando o limite de empréstimo (exemplo: 3 vezes a renda anual)
+            limite_emprestimo = renda_anual * 3
 
-            if prediction == 0:
-                st.error("Olá: De acordo aos cálculos não recebu crédito")
+            # Verificando se o montante solicitado não excede o limite
+            if duracao > limite_emprestimo:
+                st.error(f"O montante solicitado excede seu limite de empréstimo de {limite_emprestimo}.")
             else:
-                st.success("Olá: Após análise do seu cadastro você recebeu crédito")
+                # Processo de predição (ajuste conforme seu modelo)
+                features = [[genero_escolha-1, escolha_carro-1, duracao, escolha_nivel-1, escolha_estado-1, escolha_modelo-1]]
+                # Criar a predição
+                # prediction = model.predict(features)
+                
+                # condição para ter o crédito
+                prediction = 1 
 
+                if prediction == 0:
+                    st.error("Olá: De acordo aos cálculos, seu crédito não foi aprovado.")
+                else:
+                    st.success("Olá: Após análise do seu cadastro, você recebeu crédito.")
